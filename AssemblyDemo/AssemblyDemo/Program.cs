385c8822199033fc522c5eeb74a6381e2e865259
Dusernajder <dusungulu.tibi@gmail.com>
2021-01-04 00:00:03 +0100

    examining assemblies

--- AssemblyDemo/AssemblyDemo/Program.cs
+++ AssemblyDemo/AssemblyDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace AssemblyDemo
 {
@@ -6,7 +7,31 @@ namespace AssemblyDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = "/usr/local/share/dotnet/packs/Microsoft.NETCore.App.Ref/5.0.0/ref/net5.0/System.dll";
+
+            Assembly assembly = Assembly.LoadFile(path);
+            ShowAssembly(assembly);
+
+            Console.WriteLine();
+
+            Assembly currentAssembly = Assembly.GetExecutingAssembly();
+            ShowAssembly(currentAssembly);
+
+            Console.Read();
+        }
+
+        static void ShowAssembly(Assembly assembly)
+        {
+            Console.WriteLine($"Full name: {assembly.FullName}");
+            Console.WriteLine($"Global assembly: {assembly.GlobalAssemblyCache}");
+            Console.WriteLine($"Location: {assembly.Location}");
+            Console.WriteLine($"Image runtime version: {assembly.ImageRuntimeVersion}");
+
+            Console.WriteLine("Modules:");
+            foreach (Module module in assembly.GetModules())
+            {
+                Console.WriteLine(module.Name);
+            }
         }
     }
 }
