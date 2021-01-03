a0fa5a53fecbe524438bcf5c725b44e4b3f780e1
Dusernajder <dusungulu.tibi@gmail.com>
2021-01-04 00:16:54 +0100

    set assembly attributes and display them

---
+++ AssemblyDemo/AssemblyAttrDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+namespace AssemblyAttrDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly currentAssembly = Assembly.GetExecutingAssembly();
+            Type type = typeof(AssemblyDescriptionAttribute);
+            object[] customAttributes = currentAssembly.GetCustomAttributes(type, false);
+
+            if (customAttributes.Length > 0)
+            {
+                var assemblyDescriptionAttribute = (AssemblyDescriptionAttribute) customAttributes[0];
+                Console.WriteLine($"Description: {assemblyDescriptionAttribute.Description}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
